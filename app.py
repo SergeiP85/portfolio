e54d9db88596aade9ee748c2b0d0cf47a813e957
Sergei Pastushenko <sergei6463597314@gmail.com>
2025-02-18 00:21:25 -0500

    html in textarea descr

--- app.py
+++ app.py
@@ -1,33 +1,23 @@
+### app.py
 from flask import Flask
 from flask_migrate import Migrate
 from models import db
-from routes import app_routes  # Исправляем импорт
-from admin import init_admin  # Импортируем инициализацию админки
-# from models import AboutMeSection
-from flask import request, redirect, url_for, flash
+from routes import app_routes
+from admin import init_admin
 
-
-# Инициализация приложения и базы данных
 app = Flask(__name__, static_url_path='/static', static_folder='static')
-
-
-# Конфигурация приложения
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'your_secret_key'  # Измените на свой секретный ключ
+app.secret_key = 'your_secret_key'
 
 db.init_app(app)
-
 migrate = Migrate(app, db)
-
-# Подключаем маршруты
 app.register_blueprint(app_routes)
 
-# Создание базы данных и таблиц
 with app.app_context():
     db.create_all()
 
+init_admin(app)
+
 if __name__ == '__main__':
-    from admin import init_admin
-    init_admin(app)
     app.run(debug=True)
--- init_db.py
+++ init_db.py
@@ -1,16 +1,14 @@
+### init_db.py
 from app import app, db
-from models import User
+from models import User 
 from werkzeug.security import generate_password_hash
 
-# Устанавливаем контекст приложения
 with app.app_context():
-    db.create_all()  # Создать таблицы в базе данных
-    
-    # Проверяем, есть ли уже админ
+    db.create_all()
     if not User.query.filter_by(username="admin").first():
         admin_user = User(username="admin", password=generate_password_hash("secret123"))
         db.session.add(admin_user)
-        db.session.commit()  # Сохранить изменения в базе
+        db.session.commit()
         print("Админ создан")
     else:
         print("Админ уже существует")
