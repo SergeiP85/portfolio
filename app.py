3437cc26ca44828b8e8cabf1b3ae3c655ec13900
Sergei Pastushenko <sergei6463597314@gmail.com>
2025-02-21 12:40:14 -0500

    deploy test

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask
 from flask_migrate import Migrate
-from models import db  # Один импорт вместо трех
+from models import db
 from routes import app_routes
 from admin import init_admin
-import os  # Импортируем os для работы с переменными окружения
 
 print("✅ Flask загружен и работает!")
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 
-# Получаем URL базы данных из переменной окружения
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///site.db')  # Если переменная не установлена, используется SQLite
+# Указываем URL базы данных напрямую, например, SQLite:
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'your_secret_key'
+app.secret_key = 'your_secret_key'  # Секретный ключ напрямую
 
 db.init_app(app)
 migrate = Migrate(app, db)
@@ -20,5 +19,6 @@ migrate = Migrate(app, db)
 app.register_blueprint(app_routes)
 init_admin(app)
 
+# Запуск приложения
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
